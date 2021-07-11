986983b495dfc062293e43cee406ddcb52f02b7f
Jordi Jiménez Coves <jjimenezcoves@gmail.com>
2021-07-11 11:17:27 +0200

    http request

--- js/app.js
+++ js/app.js
@@ -6,9 +6,12 @@ var app = angular.module('ejemplosApp',[ ]);
 
 app.controller('mainCtrl', ['$scope','$http', function($scope,$http){
   
-  
+    $scope.profesores = {};
 
-
+    $http.get('json/profesores.json').success(function(data){
+        // Código cuando es correcta la petición
+        $scope.profesores = data.profesores;
+    });
   
 
 
